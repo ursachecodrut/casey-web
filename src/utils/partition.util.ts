7d5338c9abbc1adc028c8a30b9bc95ba99774df4
Codrut Ursache <ursache.codrut71@gmail.com>
2023-06-04 22:13:16 +0300

    fix(app): add documentation for partition function

--- src/utils/partition.util.ts
+++ src/utils/partition.util.ts
@@ -1,3 +1,16 @@
+/**
+ * Partition an array into two arrays based on a predicate.
+ * @example
+ * partition([1, 2, 3, 4, 5], (item) => item % 2 === 0);
+ * // [[2, 4], [1, 3, 5]]
+ *
+ * partition([1, 2, 3, 4, 5], (item) => item % 2 === 1);
+ * // [[1, 3, 5], [2, 4]]
+ *
+ * @param array
+ * @param predicate
+ * @returns [array, array]
+ */
 export const partition = <T>(
   array: T[],
   predicate: (item: T) => boolean
